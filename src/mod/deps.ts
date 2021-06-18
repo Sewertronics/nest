587fe87d69e23d4bb5512273f2748846e2b72750
NANOmatic <biuro@NANOmatic.pl>
2021-06-18 12:58:46 +0200

    Update deps.ts

--- src/mod/deps.ts
+++ src/mod/deps.ts
@@ -6,4 +6,4 @@ export { Tar } from "https://x.nest.land/std@0.85.0/archive/tar.ts";
 export { generate as generateUUID } from "https://x.nest.land/std@0.85.0/uuid/v4.ts";
 
 // **************** semver ****************
-export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
